79114d876a3c0ab19f442844a6c8ff54a19b9611
pius-kamau <07299kama@gmail.com>
2026-04-07 10:38:58 +0300

    Fix auth middleware exports and invoice routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const { query } = require("../config/database");
 
 const authenticate = async (req, res, next) => {
@@ -66,3 +67,17 @@ const authenticate = async (req, res, next) => {
     return res.status(401).json({ error: "Invalid token" });
   }
 };
+
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ error: "Authentication required" });
+    }
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ error: "Insufficient permissions" });
+    }
+    next();
+  };
+};
+
+module.exports = { authenticate, authorize };
